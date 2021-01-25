a27fccc09a03e396f27f41386c9501ad0eeb1247
kishordash <kishor.career@yaho.in>
2021-01-25 13:12:47 +0530

    Addded Code for Part I

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import RouterModule from './screens/RouterModule';
 
 ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
+    <RouterModule/>,
     document.getElementById('root')
+
+     
 );
